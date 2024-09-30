6789605fe7ec30204ecd4c19a7d85ca61d0608d4
SIlviaHinojosa <hinojosa.silvia@gmail.com>
2024-09-30 10:19:08 -0600

    train_snap.sh

--- train_snap.sh
+++ train_snap.sh
@@ -11,20 +11,21 @@ cp doradorun1_assembly3_reformatted.all.gff snap1/
 # Step 3: Change directory to snap1
 cd snap1/
 
-# Step 4: Change directory to SNAP location
-cd /users/shinojosa/snap/
+# Step 4: Export the SNAP path
+export SNAP_PATH="/users/shinojosa/snap"
+export PATH=$SNAP_PATH:$PATH
 
 # Step 5: Categorize the genome files
-./fathom -categorize 1000 genome.ann genome.dna
+$SNAP_PATH/fathom -categorize 1000 genome.ann genome.dna
 
 # Step 6: Export annotated files
-./fathom -export 1000 -plus uni.ann uni.dna
+$SNAP_PATH/fathom -export 1000 -plus uni.ann uni.dna
 
 # Step 7: Forge the HMM model
-./forge export.ann export.dna
+$SNAP_PATH/forge export.ann export.dna
 
 # Step 8: Assemble the HMM file using the provided perl script
-perl /users/shinojosa/snap/hmm-assembler.pl dorado . > ../dorado.hmm
+perl $SNAP_PATH/hmm-assembler.pl dorado . > ../dorado.hmm
 
 echo "SNAP training complete!"
 ###  NOTE: you might nead to type this command in terminal to make it executable chmod +x train_snap.sh ###
